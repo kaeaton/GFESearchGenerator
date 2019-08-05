7cc85ad3cf34620176d3a18c8b2cda241241a481
Katrina Eaton <katrina.eaton@gmail.com>
2019-08-05 10:29:42 -0700

    add setter methods for variable data location

--- BSG/src/main/java/org/chori/bsg/model/WhereTheDataLives.java
+++ BSG/src/main/java/org/chori/bsg/model/WhereTheDataLives.java
@@ -2,31 +2,42 @@ package org.chori.bsg.model;
 
 import java.io.File;
 import java.nio.file.*;
+import java.util.prefs.Preferences;
+
+import org.chori.bsg.view.B12xGui;
+
 
 public class WhereTheDataLives {
 
-	private final String basePath = (System.getProperty("user.home") 
+	private Preferences prefs = Preferences.userNodeForPackage(B12xGui.class);
+
+	private final String defaultBasePath = (System.getProperty("user.home") 
                 + System.getProperty("file.separator") + "Documents"
                 + System.getProperty("file.separator") + "BSG");
 
-	private final String rawDataPathAddon = (basePath + System.getProperty("file.separator")
+	private final String defaultRawDataPath = (defaultBasePath + System.getProperty("file.separator")
 				+ "BSGData" + System.getProperty("file.separator"));
 
-	public WhereTheDataLives() {
+	private String basePath = prefs.get("BSG_RESULTS_DATA", defaultBasePath);
+	private String rawDataPath = prefs.get("BSG_RAW_DATA", defaultRawDataPath);
 
+
+	public WhereTheDataLives() { }
+
+	public void setRawDataPath(String rawDataFileFolder) {
+		prefs.put("BSG_RAW_DATA", rawDataFileFolder);
+	}
+
+	public void setResultsDataPath(String resultsFileFolder) {
+		prefs.put("BSG_RESULTS_DATA", resultsFileFolder);
 	}
 
 	public void storeRawData(String locus, String version, String fileType) {
-		String rawDataPath = (basePath + rawDataPathAddon + version
-                                    + System.getProperty("file.separator") 
-                                    + locus + "_" + version + "_rawData.csv");
 		File filePath = new File(rawDataPath);
 	} 
 
 	public File getRawData(String locus, String version, String fileType) {
-		String rawDataPath = (basePath + rawDataPathAddon + version
-                                    + System.getProperty("file.separator") 
-                                    + locus + "_" + version + "_rawData.csv");
+		
 		File filePath = new File(rawDataPath);
 		return filePath;
 	}
