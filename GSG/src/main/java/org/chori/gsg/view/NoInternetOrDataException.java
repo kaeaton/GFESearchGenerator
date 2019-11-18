3fd88361ba69553ad42257ed6849d6097ad4c80f
Katrina Eaton <katrina.eaton@gmail.com>
2019-11-18 10:37:59 -0800

    add custom icon to no internet/data exception popup

--- GSG/src/main/java/org/chori/gsg/view/NoInternetOrDataException.java
+++ GSG/src/main/java/org/chori/gsg/view/NoInternetOrDataException.java
@@ -1,5 +1,7 @@
 package org.chori.gsg.view;
 
+import java.net.URL;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 
@@ -16,13 +18,25 @@ public class NoInternetOrDataException extends Exception {
 		optionFrame.setLocationRelativeTo(null);
 		optionFrame.setAlwaysOnTop(true);
 
+
+
 		// final JOptionPane optionPane = new JOptionPane(
 		JOptionPane.showMessageDialog(optionFrame,
 			"You have no internet and no downloaded data.\nI'm sorry, but this won't work.",
 			"Houston, we have a problem",
-			JOptionPane.ERROR_MESSAGE);
+			JOptionPane.ERROR_MESSAGE,
+			createImageIcon("/StrangeloveRound.png", "How I learned to quit worrying and love the bomb."));
 	}
 
-
+	/** Returns an ImageIcon, or null if the path was invalid. */
+	protected ImageIcon createImageIcon(String path, String description) {
+    	URL imgURL = getClass().getResource(path);
+    	if (imgURL != null) {
+        	return new ImageIcon(imgURL, description);
+    	} else {
+	        System.err.println("Couldn't find file: " + path);
+	        return null;
+	    }
+	}
 
 }
