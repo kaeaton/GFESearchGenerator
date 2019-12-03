f611b4403b126e2cb72ecad1b3a6747c5f884405
Katrina Eaton <katrina.eaton@gmail.com>
2019-12-03 13:00:52 -0800

    begin adding JUnit tests

--- GSG/src/test/java/SetupTest.java
+++ GSG/src/test/java/SetupTest.java
@@ -1,10 +1,27 @@
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
- 
+
+import org.chori.gsg.model.SubmissionRequestFactory.*;
+import org.chori.gsg.model.SubmissionRequests.*;
+
 public class SetupTest {
  
+	SubmissionRequestFactoryInstance submissionRequestFactoryInstance = SubmissionRequestFactoryInstance.getInstance();
+	SubmissionRequestFactory submissionRequestFactory = submissionRequestFactoryInstance.factory;
+
     @Test
-    public void justAnExample() {
- 
+    public void testSubmissionRequestFactory() {
+        System.out.println(submissionRequestFactory.assembleSubmissionRequest("ERROR", "HLA"));
+        System.out.println(submissionRequestFactory.assembleSubmissionRequest("GFE", "HLA"));
+        System.out.println(submissionRequestFactory.assembleSubmissionRequest("NAME", "HLA"));
+        System.out.println(submissionRequestFactory.assembleSubmissionRequest("FEATURE", "HLA"));
+        System.out.println(submissionRequestFactory.assembleSubmissionRequest("ERROR", "KIR"));
+        System.out.println(submissionRequestFactory.assembleSubmissionRequest("FEATURE", "ABO"));
     }
+
+ //    @Test
+ //    public void canSubmissionRequestSeeHlaDataSources() {
+	// 	SubmissionRequest testHlaNameSubmissionRequest = submissionRequestFactory.assembleSubmissionRequest("NAME", "HLA");
+	// 	System.out.println(testHlaNameSubmissionRequest.getDataSource());
+	// }
 }
