13a72cc3d090135c908cee514cb2a5b095678c15
Katrina Eaton <katrina.eaton@gmail.com>
2019-12-12 05:27:31 -0800

    add tests for SubmissionRequestFactory and dataSources

--- GSG/src/test/java/SetupTest.java
+++ GSG/src/test/java/SetupTest.java
@@ -1,4 +1,4 @@
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 import org.junit.Test;
 
 import org.chori.gsg.model.SubmissionRequestFactory.*;
@@ -6,23 +6,35 @@ import org.chori.gsg.model.SubmissionRequests.*;
 
 public class SetupTest {
  
+
 	SubmissionRequestFactoryInstance submissionRequestFactoryInstance = SubmissionRequestFactoryInstance.getInstance();
 	SubmissionRequestFactory submissionRequestFactory = submissionRequestFactoryInstance.factory;
+	HlaGfeSubmissionRequest hlaGfeSubReq = new HlaGfeSubmissionRequest();
+	HlaNameSubmissionRequest hlaNameSubReq = new HlaNameSubmissionRequest();
+	HlaFeatureSubmissionRequest hlaFeatureSubReq = new HlaFeatureSubmissionRequest();
 
-    @Test
-    public void testSubmissionRequestFactory() {
-        System.out.println(submissionRequestFactory.assembleSubmissionRequest("ERROR", "HLA"));
-        System.out.println(submissionRequestFactory.assembleSubmissionRequest("GFE", "HLA"));
-        System.out.println(submissionRequestFactory.assembleSubmissionRequest("NAME", "HLA"));
-        System.out.println(submissionRequestFactory.assembleSubmissionRequest("FEATURE", "HLA"));
-        System.out.println(submissionRequestFactory.assembleSubmissionRequest("ERROR", "KIR"));
-        System.out.println(submissionRequestFactory.assembleSubmissionRequest("FEATURE", "ABO"));
-    }
 
-    @Test
-    public void canSubmissionRequestSeeHlaDataSources() {
-		SubmissionRequest testHlaNameSubmissionRequest = submissionRequestFactory.assembleSubmissionRequest("NAME", "HLA");
-		String dataSource = testHlaNameSubmissionRequest.getDataSource();
-		System.out.println(dataSource);
+	@Test
+	public void testSubmissionRequestFactory() {
+		assertSame("The submission request objects do not match, HLA, GFE", hlaGfeSubReq.getClass(),
+						submissionRequestFactory.assembleSubmissionRequest("HLA", "GFE").getClass());
+		assertSame("The submission request objects do not match, HLA, NAME", hlaNameSubReq.getClass(),
+						submissionRequestFactory.assembleSubmissionRequest("HLA", "NAME").getClass());
+		assertSame("The submission request objects do not match, HLA, FEATURE", hlaFeatureSubReq.getClass(),
+						submissionRequestFactory.assembleSubmissionRequest("HLA", "FEATURE").getClass());
+		assertSame("The submission request failures do not match, HLA, ERROR", null,
+						submissionRequestFactory.assembleSubmissionRequest("HLA", "ERROR"));
+		assertSame("The submission request errors do not match, KIR, ERROR", null,
+						submissionRequestFactory.assembleSubmissionRequest("KIR", "ERROR"));
+		assertSame("The submission request errors do not match, ABO, FEATURE", null,
+						submissionRequestFactory.assembleSubmissionRequest("ABO", "FEATURE"));
+	}
+
+	@Test
+	public void canSubmissionRequestSeeDataSources() {
+		assertEquals("http://neo4j.b12x.org", hlaGfeSubReq.dataSource);
+		assertEquals("http://neo4j.b12x.org", hlaNameSubReq.dataSource);
+		assertEquals("http://neo4j.b12x.org", hlaFeatureSubReq.dataSource);
+		
 	}
 }
